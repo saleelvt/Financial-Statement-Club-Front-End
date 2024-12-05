4e80fe0dc5b48a8da8020485905184034ca68e08
saleel vt <saleelvt57@gmail.com>
2024-12-05 17:46:56 +0530

    add: security added

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,6 +5,7 @@ export const URL="https://api.finstatements.club";
 export const createAxiosConfig = (isFileUpload = false) => ({
     headers: {
         "Content-Type": isFileUpload ? "multipart/form-data" : "application/json",
+         "X-Requested-With": "XMLHttpRequest"
     },
     withCredentials: true,
 });

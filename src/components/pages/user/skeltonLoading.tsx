29e3f36fc1d88da9b0a8515d498a1d97cf0edb3c
saleel vt <saleelvt57@gmail.com>
2025-05-21 16:09:30 +0530

    sds

--- src/components/pages/user/skeltonLoading.tsx
+++ src/components/pages/user/skeltonLoading.tsx
@@ -12,7 +12,7 @@ const SkeltonLoading: React.FC = React.memo(() => {
 
     <div className=" px-44 flex justify-center items-center min-h-screen bg-gradient-to-b from-gray-300 via-gray-100 to-white ">
     
-      <div className="grid mt-24 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 w-full max-w-7xl">
+      <div className="grid mt-24 grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-6 gap-6 w-full max-w-7xl">
         {[...Array(columns)].map((_, colIndex) => (
           <div key={colIndex} className="flex p-1  flex-col gap-3">
             {[...Array(itemsPerColumn)].map((_, itemIndex) => (
